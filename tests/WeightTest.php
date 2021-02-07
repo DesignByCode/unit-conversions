349cea982d998964bfca9161bced1fd55e9347b0
Claude Myburgh <claudemyburgh@gmail.com>
2021-02-07 20:58:22 +0200

    Update weight test

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -8,9 +8,17 @@ use PHPUnit\Framework\TestCase;
 class WeightTest extends TestCase
 {
     /** @test */
-    public function it_can_convert_kilograms_to_lbs()
+    public function it_can_convert_kilograms_to_lbs_static()
     {
         $lbs = Weight::fromKilograms(100)->toLbs();
+
+        $this->assertEquals(220.4623, $lbs);
+    }
+
+    /** @test */
+    public function it_can_convert_kilograms_to_lbs()
+    {
+        $lbs = (new Weight(100))->toLbs();
 
         $this->assertEquals(220.4623, $lbs);
     }
